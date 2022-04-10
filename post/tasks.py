d94fe5910f4152638c730a16225da27d19944829
darkhan <chapaid8@gmail.com>
2022-04-10 23:40:14 +0600

    Add logger to scheduler task

--- post/tasks.py
+++ post/tasks.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -5,9 +6,12 @@ from apscheduler.triggers.interval import IntervalTrigger
 
 scheduler = BlockingScheduler()
 
+logger = logging.getLogger(__name__)
+
 
 @scheduler.scheduled_job(IntervalTrigger(seconds=3))
 def train_model():
+    logger.info('Book %s is exported', datetime.now())
     print('dask train_model! The time is: %s' % datetime.now())
 
 
