63c7cd4f83108799b3b8d5e44ed0bc7cab5c537b
darkhan <chapaid8@gmail.com>
2022-04-10 14:25:15 +0600

    Add static dic

--- news/settings.py
+++ news/settings.py
@@ -133,10 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # All settings common to all environments
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
