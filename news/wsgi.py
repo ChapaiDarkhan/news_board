3080554d2013f22980c13f27ea3c64b1e314b478
darkhan <chapaid8@gmail.com>
2022-04-11 01:08:02 +0600

    Add scheduler task

--- news/wsgi.py
+++ news/wsgi.py
@@ -8,9 +8,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news.settings')
-
-application = get_wsgi_application()
+import sys
+DJANGO_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+sys.path.append(DJANGO_PATH)
--- tasks.py
+++ tasks.py
@@ -1,16 +1,18 @@
-import logging
-from post.models import Post
+import os
+import django
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
 scheduler = BlockingScheduler()
-
-logger = logging.getLogger(__name__)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news.settings")
+django.setup()
 
 
 @scheduler.scheduled_job(IntervalTrigger(seconds=3))
 def update_posts_votes():
-    queryset = Post.objects.all().update(votes_number=0)
+    from post import models
+    print("here")
+    models.Post.objects.all().update(votes_number=0)
 
 
 scheduler.start()
